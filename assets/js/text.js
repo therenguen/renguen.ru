73fe2e14db99727f46f21a1616f41605233a9664
Andrey <the@renguen.ru>
2025-02-27 13:31:53 +0300

    meow

--- assets/js/text.js
+++ assets/js/text.js
@@ -1,6 +1,6 @@
 $(function () {
     $(".typed").typed({
-        strings: ["Что-то между прогером и скрипт-кидди", "RT", "ANTI-SCAM", "Python Developer", "coder", "vzlom jopi", "hmmm...", "bruh", "KR4CK3", "Pashalko", "h@Cк3D!", "#1151"],
+        strings: ["Python", "Kotlin beginner", "Kitty!!!"],
         typeSpeed: 30,
         backSpeed: 0,
         cursorChar: ['❚'],
